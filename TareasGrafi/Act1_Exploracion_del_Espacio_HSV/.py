e5c3c5b215e2aed711bb7e9c3a46907611dc3262
Yahir <yahir@norelia.com>
2026-03-12 16:18:44 -0600

    Act1_Exploracion_del_Espacio_HSV ACTUALIZADA CON SU DOCUMENTACION EN .MD

--- TareasGrafi/Act1_Exploracion_del_Espacio_HSV/.py
+++ TareasGrafi/Act1_Exploracion_del_Espacio_HSV/.py
@@ -7,15 +7,19 @@ img = cv2.imread("C:\\Users\\Asus\\OneDrive\\Documentos\\GRAFICACION\\TareasGraf
 # convertir a HSV
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-# rango para color rojo
+# rango HSV para color rojo
 lower_red = np.array([0,120,70])
 upper_red = np.array([10,255,255])
 
+# crear mascara para color rojo
 mask_red = cv2.inRange(hsv, lower_red, upper_red)
 
+# mostrar resultados
 cv2.imshow("Imagen Original", img)
 cv2.imshow("Imagen HSV", hsv)
 cv2.imshow("Mascara Roja", mask_red)
 
+# Esperar hasta que se presione una tecla
 cv2.waitKey(0)
+# Cerar ventanas
 cv2.destroyAllWindows()
